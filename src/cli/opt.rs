e0cf97cec3df4df55a4c2abbefda5278f7ca3884
ElonH <elonhhuang@gmail.com>
2021-09-21 18:02:56 +0800

    Opt: validation func

--- src/cli/opt.rs
+++ src/cli/opt.rs
@@ -1,12 +1,18 @@
+use regex::Regex;
 pub use structopt::StructOpt;
-
 
 /// The options of the CLI of UNM (Rust)
 #[derive(StructOpt, Debug)]
 #[structopt(name = "unm-server-rust")]
 pub struct Opt {
     /// Specify the server port of UNM.
-    #[structopt(short, long, default_value = "8080;8081", use_delimiter=true, value_delimiter=";")]
+    #[structopt(
+        short,
+        long,
+        default_value = "8080;8081",
+        use_delimiter = true,
+        value_delimiter = ";"
+    )]
     pub port: Vec<u16>,
 
     /// Specify the server host of UNM.
@@ -32,4 +38,31 @@ pub struct Opt {
     /// Enable the proxy limitation. (Not implemented)
     #[structopt(short, long)]
     pub strict: bool,
+}
+
+impl Opt {
+    pub fn is_valid(&self) -> Option<String> {
+        self.proxy_url
+            .as_ref()
+            .and_then(|url| {
+                let proxy_url_re: Regex =
+                    Regex::new(r"^http(s?):\/\/.+:\d+$").expect("wrong regex of proxy url");
+                match proxy_url_re.is_match(&url) {
+                    true => None,
+                    false => Some(String::from("lease check the proxy url.")),
+                }
+            })
+            .or(self.endpoint.as_ref().and_then(|url| {
+                let re = Regex::new(r"^http(s?):\/\/.+$").expect("wrong regex of endpoint");
+                match re.is_match(&url) {
+                    true => None,
+                    false => Some(String::from("Please check the endpoint host.")),
+                }
+            }))
+            .or(self
+                .host
+                .parse::<std::net::IpAddr>()
+                .err()
+                .map(|_| String::from("Please check the server host.")))
+    }
 }
