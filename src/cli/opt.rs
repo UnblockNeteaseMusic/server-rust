499c1b434e655422fa313d32333b147d365e7327
pan93412 <pan93412@gmail.com>
2021-09-24 16:16:27 +0800

    refactor(providers): separate providers from cli/opt

--- src/cli/opt.rs
+++ src/cli/opt.rs
@@ -1,58 +1,8 @@
 use crate::logger::LevelFilter;
+use crate::providers::Provider;
 use regex::Regex;
 use std::path::PathBuf;
 pub use structopt::StructOpt;
-
-#[derive(StructOpt, PartialEq, Clone)]
-/// The source provider
-pub enum Provider {
-    QQ,
-    Kugou,
-    Kuwo,
-    Migu,
-    Joox,
-    Youtube,
-    YoutubeDL,
-    Bilibili,
-    Pyncmd,
-}
-
-impl std::str::FromStr for Provider {
-    type Err = String;
-    fn from_str(src: &str) -> Result<Self, Self::Err> {
-        match src {
-            "qq" => Ok(Provider::QQ),
-            "kugou" => Ok(Provider::Kugou),
-            "kuwo" => Ok(Provider::Kuwo),
-            "migu" => Ok(Provider::Migu),
-            "joox" => Ok(Provider::Joox),
-            "youtube" => Ok(Provider::Youtube),
-            "youtubedl" => Ok(Provider::YoutubeDL),
-            "bilibili" => Ok(Provider::Bilibili),
-            "pyncmd" => Ok(Provider::Pyncmd),
-            _ => Err(String::from(format!("{}", src))),
-        }
-    }
-}
-impl std::fmt::Debug for Provider {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match &self {
-                Provider::QQ => "qq",
-                Provider::Kugou => "kugou",
-                Provider::Kuwo => "kuwo",
-                Provider::Migu => "migu",
-                Provider::Joox => "joox",
-                Provider::Youtube => "youtube",
-                Provider::YoutubeDL => "youtubedl",
-                Provider::Bilibili => "bilibili",
-                Provider::Pyncmd => "pyncmd",
-            }
-        )
-    }
-}
 
 fn parse_bool(src: &str) -> Result<bool, &str> {
     return if src == "0" || src == "false" {
@@ -61,7 +11,7 @@ fn parse_bool(src: &str) -> Result<bool, &str> {
         Ok(true)
     } else {
         Err("provided string was not `true`, `false`, `0` or `1`")
-    }
+    };
 }
 
 #[derive(StructOpt, PartialEq, Clone, Debug)]
---
+++ src/providers.rs
@@ -0,0 +1,51 @@
+#[derive(PartialEq, Clone)]
+/// The source provider
+pub enum Provider {
+    QQ,
+    Kugou,
+    Kuwo,
+    Migu,
+    Joox,
+    Youtube,
+    YoutubeDL,
+    Bilibili,
+    Pyncmd,
+}
+
+impl std::str::FromStr for Provider {
+    type Err = String;
+    fn from_str(src: &str) -> Result<Self, Self::Err> {
+        match src {
+            "qq" => Ok(Provider::QQ),
+            "kugou" => Ok(Provider::Kugou),
+            "kuwo" => Ok(Provider::Kuwo),
+            "migu" => Ok(Provider::Migu),
+            "joox" => Ok(Provider::Joox),
+            "youtube" => Ok(Provider::Youtube),
+            "youtubedl" => Ok(Provider::YoutubeDL),
+            "bilibili" => Ok(Provider::Bilibili),
+            "pyncmd" => Ok(Provider::Pyncmd),
+            _ => Err(String::from(format!("{}", src))),
+        }
+    }
+}
+
+impl std::fmt::Debug for Provider {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match &self {
+                Provider::QQ => "qq",
+                Provider::Kugou => "kugou",
+                Provider::Kuwo => "kuwo",
+                Provider::Migu => "migu",
+                Provider::Joox => "joox",
+                Provider::Youtube => "youtube",
+                Provider::YoutubeDL => "youtubedl",
+                Provider::Bilibili => "bilibili",
+                Provider::Pyncmd => "pyncmd",
+            }
+        )
+    }
+}
