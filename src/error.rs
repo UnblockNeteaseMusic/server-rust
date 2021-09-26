2e4d81a939a8fe6763f900bf7624889aff77dd36
ElonH <elonhhuang@gmail.com>
2021-09-26 23:16:46 +0800

    add url::ParseError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 pub use reqwest::Error as ReqErr;
 pub use serde_json::Error as JsonErr;
 use thiserror::Error as BaseErr;
+pub use url::ParseError as UrlErr;
 
 #[derive(BaseErr, Debug)]
 pub enum Error {
@@ -10,6 +11,8 @@ pub enum Error {
     HeadersDataInvalid,
     #[error("Failed to parse JSON: {0}")]
     JsonParseFail(JsonErr),
+    #[error("Failed to parse URL: {0}")]
+    UrlParseFail(UrlErr),
     #[error("Failed to XOR this ID char (u32) {0} with this key char (u32) {1}")]
     UriEncryptXorFail(u32, u32),
     #[error("Error storing unknown data.")]
