b13c004a102d83c78956cf5a4806780dfee0c4dc
pan93412 <pan93412@gmail.com>
2021-10-01 02:19:17 +0800

    feat(unm_macro): add the macro "is_host_wrapper"

---
+++ unm_macro/src/lib.rs
@@ -0,0 +1,17 @@
+#[macro_export]
+macro_rules! is_host_wrapper {
+    ($($item: expr),+) => {
+        |host: &&str| [$($item),+].contains(host)
+    };
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn is_host_wrapper_test() {
+        let is_host = is_host_wrapper!("a", "b");
+        assert!(is_host(&"a"));
+        assert!(is_host(&"b"));
+        assert!(!is_host(&"c"));
+    }
+}
