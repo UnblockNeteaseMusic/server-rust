b609d8fa92a36a3b8df8fbb53a3dd1d981b34326
pan93412 <pan93412@gmail.com>
2021-10-01 18:55:30 +0800

    refactor(main): clean up main()

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,29 @@ use unm_server::{
     request::proxy::ProxyManager,
 };
 
-fn main() -> Result<()> {
+fn init_opt() -> Opt {
     let opt: Opt = Opt::from_args();
 
-    match opt.arg_check() {
-        None => {}
-        Some(msg) => {
-            println!("{}", msg);
-        }
+    if let Some(msg) = opt.arg_check() {
+        panic!("\x1b[1;31mERROR:\x1b[0m {}", msg);
     }
 
-    init_logger(&opt)?;
+    opt
+}
+
+fn init_proxy_manager(opt: &Opt) -> Result<ProxyManager> {
     let mut proxy_manager = ProxyManager { proxy: None };
     if let Some(url) = &opt.proxy_url {
         proxy_manager.setup_proxy(url)?;
     };
+
+    Ok(proxy_manager)
+}
+
+fn main() -> Result<()> {
+    let opt = init_opt();
+    init_logger(&opt)?;
+    let proxy_manager = init_proxy_manager(&opt)?;
 
     info!("Info log!");
     warn!("Warn log with value {}", "tests");
