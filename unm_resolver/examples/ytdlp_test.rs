771ed5e4f54aa830e42c440f63353baea1ba931b
pan93412 <pan93412@gmail.com>
2022-01-16 17:23:30 +0800

    chore(unm_resolver/engine): add example

---
+++ unm_resolver/examples/ytdlp_test.rs
@@ -0,0 +1,21 @@
+use unm_resolver::engine::{Engine, Song, Artist};
+
+#[tokio::main]
+async fn main() {
+    let ytdlp = unm_resolver::engine::ytdlp::YtDlpEngine;
+    let song = Song {
+        name: "青花瓷".to_string(),
+        artists: vec![Artist {
+            name: "周杰伦".to_string(),
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+
+    let start = std::time::Instant::now();
+    let result = ytdlp.check(&song, None).await.unwrap();
+    let end = start.elapsed();
+
+    println!("周杰伦 - 青花瓷: {:?}", result);
+    println!("Time taken: {:?}", end);
+}
