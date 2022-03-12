98ea238deb2a33b5f02da10d148ef097f18f59d1
pan93412 <pan93412@gmail.com>
2022-03-12 21:03:22 +0800

    feat: add example for new search and retrieve

---
+++ unm_resolver/examples/search_retrieve_test.rs
@@ -0,0 +1,48 @@
+use futures::FutureExt;
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+use unm_resolver::{
+    engine::{Artist, Context, Song},
+    resolve::{batch_search, retrieve, Engine},
+};
+use unm_test_utils::measure_async_function_time;
+
+#[tokio::main]
+async fn main() {
+    SimpleLogger::new()
+        .with_utc_timestamps()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
+
+    let song = Song {
+        name: "青花瓷".to_string(),
+        artists: vec![Artist {
+            name: "周杰伦".to_string(),
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+
+    let engines = [Engine::Bilibili, Engine::YtDlp, Engine::YtDl, Engine::Migu];
+    let context = Context::default();
+
+
+    let (search_time_taken, search_result) = measure_async_function_time(|| {
+        batch_search(
+            &engines,
+            &song,
+            &context,
+        )
+        .boxed()
+    })
+    .await;
+    let search_result = search_result.expect("should has a search result");
+
+    let (retrieve_time_taken, retrieved_result) =
+        measure_async_function_time(|| retrieve(&search_result, &context).boxed()).await;
+    let retrieved_result = retrieved_result.expect("can't be retrieved");
+
+    println!("[Retrieved] 周杰伦 - 青花瓷: {} (from {})", retrieved_result.url, retrieved_result.source);
+    println!("Search taken {:?} while retrieve tooke {:?}.", search_time_taken, retrieve_time_taken);
+}
