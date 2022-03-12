57cd7b4d10e91f993cce06842ff34204c8d190cf
pan93412 <pan93412@gmail.com>
2022-03-12 22:02:27 +0800

    feat: use engine_example_wrapper for examples

--- unm_resolver/examples/bilibili_test.rs
+++ unm_resolver/examples/bilibili_test.rs
@@ -1,21 +1,4 @@
-use unm_resolver::engine::{Artist, Context, Engine, Song};
-
 #[tokio::main]
 async fn main() {
-    let engine = unm_resolver::engine::bilibili::BilibiliEngine;
-    let song = Song {
-        name: "青花瓷".to_string(),
-        artists: vec![Artist {
-            name: "周杰伦".to_string(),
-            ..Default::default()
-        }],
-        ..Default::default()
-    };
-
-    let start = std::time::Instant::now();
-    let result = engine.check(&song, &Context::default()).await.unwrap();
-    let end = start.elapsed();
-
-    println!("周杰伦 - 青花瓷: {:?}", result);
-    println!("Time taken: {:?}", end);
+    unm_test_utils::engine_example_wrapper(unm_resolver::engine::bilibili::BilibiliEngine).await;
 }
