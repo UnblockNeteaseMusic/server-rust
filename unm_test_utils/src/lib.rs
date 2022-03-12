acd192699f0dc41849bc30297f398c62095e9285
pan93412 <pan93412@gmail.com>
2022-03-12 22:02:16 +0800

    feat: engine_example_wrapper

--- unm_test_utils/src/lib.rs
+++ unm_test_utils/src/lib.rs
@@ -1,4 +1,6 @@
 use std::{time::Duration, future::Future, pin::Pin};
+use unm_resolver::{engine::{Engine, Artist, Context}, resolve::Song};
+use futures::FutureExt;
 
 /// Measure the time taken by the given closure.
 #[inline]
@@ -16,4 +18,36 @@ pub async fn measure_async_function_time<'a, T>(func: impl Fn() -> Pin<Box<dyn F
     let response = func().await;
 
     (start.elapsed(), response)
+}
+
+pub async fn engine_example_wrapper(engine: impl Engine) {
+    let song = Song {
+        name: "青花瓷".to_string(),
+        artists: vec![Artist {
+            name: "周杰伦".to_string(),
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+    let context = Context::default();
+
+    let (search_time_taken, search_result) = measure_async_function_time(|| {
+        engine.search(
+            &song,
+            &context,
+        )
+        .boxed()
+    })
+    .await;
+    let identifier = search_result
+        .expect("failed to search")
+        .expect("should has a search result")
+        .identifier;
+
+    let (retrieve_time_taken, retrieved_result) =
+        measure_async_function_time(|| engine.retrieve(&identifier, &context).boxed()).await;
+    let retrieved_result = retrieved_result.expect("can't be retrieved");
+
+    println!("[Retrieved] 周杰伦 - 青花瓷: {} (from {})", retrieved_result.url, retrieved_result.source);
+    println!("Search taken {:?} while retrieve took {:?}.", search_time_taken, retrieve_time_taken);
 }
