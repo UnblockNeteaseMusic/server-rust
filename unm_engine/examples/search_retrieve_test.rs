516d327ff7813871012d18b9bb27cb98458e08b9
pan93412 <pan93412@gmail.com>
2022-04-06 19:18:57 +0800

    feat(engine/example): add bilibili source

--- unm_engine/examples/search_retrieve_test.rs
+++ unm_engine/examples/search_retrieve_test.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use futures::FutureExt;
 use unm_test_utils::{measure_async_function_time, set_logger};
 use unm_types::{Artist, Context, Song};
@@ -17,12 +19,13 @@ async fn main() {
     let context = Context::default();
     let executor = {
         let mut e = unm_engine::executor::Executor::new();
+        e.register(unm_engine_bilibili::ENGINE_ID, Arc::new(unm_engine_bilibili::BilibiliEngine));
         // e.register(, Arc::new(unm_engine::engine::BilibiliEngine::default()));
         e
     };
 
     let (search_time_taken, search_result) =
-        measure_async_function_time(|| executor.search(&[], &song, &context).boxed()).await;
+        measure_async_function_time(|| executor.search(&[unm_engine_bilibili::ENGINE_ID], &song, &context).boxed()).await;
     let search_result = search_result.expect("should has a search result");
 
     let (retrieve_time_taken, retrieved_result) =
