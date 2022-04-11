ff6cd2db0b91d3de95b7a8d53dae46a47fe56f72
pan93412 <pan93412@gmail.com>
2022-04-11 16:30:05 +0800

    fix(engine/migu): remove reduntant {}

--- unm_engine_migu/src/lib.rs
+++ unm_engine_migu/src/lib.rs
@@ -110,7 +110,7 @@ impl Engine for MiguEngine {
 /// which the `keyword` will be encoded and trimmed.
 fn construct_search_api(keyword: &str) -> anyhow::Result<Url> {
     Ok(Url::parse_with_params(
-        r#"https://pd.musicapp.migu.cn/MIGUM2.0/v1.0/content/search_all.do?&ua=Android_migu&version=5.0.1&pageNo=1&pageSize=10&searchSwitch={{"song":1,"album":0,"singer":0,"tagSong":0,"mvSong":0,"songlist":0,"bestShow":1}}"#,
+        r#"https://pd.musicapp.migu.cn/MIGUM2.0/v1.0/content/search_all.do?&ua=Android_migu&version=5.0.1&pageNo=1&pageSize=10&searchSwitch={"song":1,"album":0,"singer":0,"tagSong":0,"mvSong":0,"songlist":0,"bestShow":1}"#,
         &[
             ("text", keyword.trim()),
         ]
