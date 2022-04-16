c6257015612c0f97fd3dbfda4a4f7703292dbae8
pan93412 <pan93412@gmail.com>
2022-04-16 16:56:18 +0800

    feat(scripts/check): add check script

---
+++ scripts/check.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+echo "Checking before publishing…"
+
+echo "  → Updating dependencies"
+cargo update --workspace && cargo upgrade --workspace || exit 1
+
+echo "  → Check & Test"
+cargo check && cargo test || exit 1
+
+echo "  → Format"
+cargo fmt || exit 1
+
+
+pushd napi || exit 1
+
+echo "  → Upgrading N-API dependencies"
+yarn upgrade -L || exit 1
+
+echo "  → Rebuild N-API"
+yarn build || exit 1
+
+popd || exit 1
+
+
+echo "Done!"
