d6e0ee6763606ef528b5cc3578bca6cafa6a4580
pan93412 <pan93412@gmail.com>
2022-04-17 16:08:19 +0800

    feat(api-utils): add API utilities

---
+++ api-utils/src/lib.rs
@@ -0,0 +1,13 @@
+//! UnblockNeteaseMusic (Rust)'s API Utilities.
+//!
+//! The API here can help you build APIs around UNM easily.
+//! For example, you can build executors with all supported engines
+//! quickly with the utilities here.
+//!
+//! # example
+//!
+//! ```
+//! use unm_api_utils::
+//! ```
+
+pub mod executor;
